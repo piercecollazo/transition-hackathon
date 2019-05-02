dc04c53eb4af44bc440bbe5ff36b48f407ed424d
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-05-02 15:24:30 -0400

    This mess is cray, though I do have a basic proof of concept laid out. needs controls

---
+++ friendlies.js
@@ -0,0 +1,19 @@
+const carrier = {
+    hp: 100,
+    attack: 50,
+}
+
+const battleship = {
+    hp: 200,
+    attack: 35,
+}
+
+const destroyer = {
+    hp: 100,
+    attack: 20,
+}
+
+const submarine = {
+    hp: 50,
+    attack: 100,
+}
